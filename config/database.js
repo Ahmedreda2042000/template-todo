827dd2204ee6fba23feec12a5e13d1668df32e29
unknown <ahmedreda2042000@gmail.com>
2023-11-29 17:07:53 +0200

    the new edit

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE;
